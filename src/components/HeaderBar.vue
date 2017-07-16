9bed5e93db435255a989b7b35c3b07990c99ea23
jianglijun <jianglijun1004@163.com>
2017-07-16 10:20:18 +0800

    write your meaaage

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,36 @@
+<template>
+<!--顶部bar-->
+<mu-appbar class="HeaderBar">
+<img class="logo" src="../assets/logo.png">
+<!--发布帖子 ===未完成-->
+<mu-icon-button  icon="send" to="/vue-home/dist/publish" slot="right"/>  
+</mu-appbar>
+</template>
+
+<style scoped>
+.HeaderBar{
+  position:fixed;
+  top: 0;
+  height: 4rem;
+}
+.appbar-search-field {
+  color: #FFF;
+  margin-bottom: 0;
+}
+.appbar-search-field.focus-state {
+  color: #FFF;
+}
+.appbar-search-field .mu-text-field-hint {
+  color: rgba(255, 255, 255, 0.54);
+}
+.appbar-search-field .mu-text-field-input {
+  color: #FFF;
+}
+.logo{
+  width:4rem;
+  height:4rem;
+  position:absolute;
+  left:0;
+  top:0;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+<!--首页-->
+    <div class="wrapper">
+        <!--顶栏-->
+        <HeaderBar></HeaderBar>
+        <!--顶部的tabs及列表内容-->
+        <HeaderTabs class="tabs"></HeaderTabs>
+        <!--底部导航-->
+        <FooterNav></FooterNav>
+    </div>
+</template>
+
+<script>
+    import HeaderBar from '../components/HeaderBar.vue'
+    import HeaderTabs from '../components/HeaderTabs.vue'
+    import FooterNav from '../components/FooterNav.vue'
+    import axios from 'axios'
+    export default {
+        components: {
+            FooterNav,
+            HeaderBar,
+            HeaderTabs
+        },
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;  
+        .tabs {
+            flex: 1;
+            margin: 8rem 0 5rem 0;
+        }
+    }
+</style>
